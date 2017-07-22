cde9b4e8d3147d3a5f4189dc808a8c0e22d1e481
Usurer <ivvlg@narod.ru>
2017-07-22 15:43:42 +0300

    Use my OnValidation handler for CookieAuthProvider. I was struggling because of getting null UserManager from OwinContext during OnValidation handler execution - it turned out that I had an error in Owin pipeline config - see change in Configuration method.

---
+++ Empty/Identity/MyTimeStampValidator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security.Cookies;
+
+namespace Empty.Identity
+{
+    // Should be MySecurityStampValidator
+    public class MyTimeStampValidator
+    {
+        public static Func<CookieValidateIdentityContext, Task> OnValidateIdentity<TManager, TUser>(TimeSpan validateInterval, Func<TManager, TUser, Task<ClaimsIdentity>> regenerateIdentity) where TManager : UserManager<TUser, string> where TUser : class, IUser<string>
+        {
+            return OnValidateIdentity<TManager, TUser, string>(validateInterval, regenerateIdentity, (Func<ClaimsIdentity, string>)(id => id.GetUserId()));
+        }
+
+        public static Func<CookieValidateIdentityContext, Task> OnValidateIdentity<TManager, TUser, TKey>(TimeSpan validateInterval, Func<TManager, TUser, Task<ClaimsIdentity>> regenerateIdentityCallback, Func<ClaimsIdentity, TKey> getUserIdCallback) where TManager : UserManager<TUser, TKey> where TUser : class, IUser<TKey> where TKey : IEquatable<TKey>
+        {
+            if (getUserIdCallback == null)
+                throw new ArgumentNullException("getUserIdCallback");
+            return (Func<CookieValidateIdentityContext, Task>)(async context =>
+            {
+                DateTimeOffset currentUtc = DateTimeOffset.UtcNow;
+                if (context.Options != null && context.Options.SystemClock != null)
+                    currentUtc = context.Options.SystemClock.UtcNow;
+                DateTimeOffset? issuedUtc = context.Properties.IssuedUtc;
+                bool validate = !issuedUtc.HasValue;
+                if (issuedUtc.HasValue)
+                    validate = currentUtc.Subtract(issuedUtc.Value) > validateInterval;
+                if (!validate)
+                    return;
+                TManager manager = context.OwinContext.GetUserManager<TManager>();
+                TKey userId = getUserIdCallback(context.Identity);
+                if ((object)manager == null || (object)userId == null)
+                    return;
+                TUser user = await manager.FindByIdAsync(userId);
+                bool reject = true;
+                if ((object)user != null && manager.SupportsUserSecurityStamp)
+                {
+                    string securityStamp = context.Identity.FindFirstValue("AspNet.Identity.SecurityStamp");
+                    if (securityStamp == await manager.GetSecurityStampAsync(userId))
+                    {
+                        reject = false;
+                        if (regenerateIdentityCallback != null)
+                        {
+                            ClaimsIdentity identity = await regenerateIdentityCallback(manager, user);
+                            if (identity != null)
+                            {
+                                context.Properties.IssuedUtc = new DateTimeOffset?();
+                                context.Properties.ExpiresUtc  = new DateTimeOffset?();
+                                context.OwinContext.Authentication.SignIn(context.Properties, new ClaimsIdentity[1]
+                                {
+                                    identity
+                                });
+                            }
+                        }
+                    }
+                }
+                if (!reject)
+                    return;
+                context.RejectIdentity();
+                context.OwinContext.Authentication.SignOut(new string[1]
+                {
+                    context.Options.AuthenticationType
+                });
+            });
+        }
+
+    }
+}
--- Empty/Startup.cs
+++ Empty/Startup.cs
@@ -14,9 +14,10 @@ namespace Empty
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
             app.CreatePerOwinContext<MyUserManager>(() => new MyUserManager(new MyUserStore()));
             app.CreatePerOwinContext<MySignInManager>(MySignInManager.Create);
+
+            ConfigureAuth(app);
         }
 
         public void ConfigureAuth(IAppBuilder app)
@@ -25,11 +26,16 @@ namespace Empty
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Login/Index"),
-                /*Provider = new MyCookieAuthenticationProvider
+                Provider = new MyCookieAuthenticationProvider
                 {
-                    OnValidateIdentity = (x) =>
+                    OnValidateIdentity = /*MyTimeStampValidator.OnValidateIdentity<MyUserManager, MyUser>(
+                            validateInterval: TimeSpan.FromSeconds(1),
+                            regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))*/
+
+                    (x) =>
                     {
                         var userManager = x.OwinContext.GetUserManager<MyUserManager>();
+
                         var userId = x.Identity.GetUserId();
                         var foundUser = userManager.FindById(userId);
                         if (foundUser != null)
@@ -44,15 +50,15 @@ namespace Empty
 
                         return Task.CompletedTask;
                     }
-                }*/
-                Provider = new CookieAuthenticationProvider
+                }
+                /*Provider = new CookieAuthenticationProvider
                 {
                     // Enables the application to validate the security stamp when the user logs in.
                     // This is a security feature which is used when you change a password or add an external login to your account.  
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<MyUserManager, MyUser>(
-                        validateInterval: TimeSpan.FromMinutes(30),
+                    OnValidateIdentity = MyTimeStampValidator.OnValidateIdentity<MyUserManager, MyUser>(
+                        validateInterval: TimeSpan.FromSeconds(1),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
-                }
+                }*/
             });
         }
     }
